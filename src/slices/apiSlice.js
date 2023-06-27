109b46464977d70a288a04903dae411bfd127886
Iryna Yudina <yudinaira4444@gmail.com>
2023-06-27 18:48:42 +0300

    credential true

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -2,6 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({
   baseUrl: "https://codeeditorbackend-production.up.railway.app/",
+  credentials: "include",
 });
 
 export const apiSlice = createApi({
