9e5b1803454334bc0ed712189452be135d6e5aec
Iryna Yudina <yudinaira4444@gmail.com>
2023-07-06 21:27:21 +0300

    Update CollabEditor.js

--- src/pages/Collaboratory/CollabEditor.js
+++ src/pages/Collaboratory/CollabEditor.js
@@ -294,6 +294,9 @@ export default function CollabEditor() {
     q.setText("Loading...");
     setQuill(q);
   }, []);
+
+useEffect(()=>{console.log(owners)}, [owners]);
+  
   return (
     <div>
       <div className="top-collab-nav">
